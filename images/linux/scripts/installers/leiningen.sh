6334cab43c88f9fbdf8e524cee8d8a9c12e49867
Sergey Dolin <v-sedoli@microsoft.com>
2018-09-13 12:23:15 +0300

    add LEIN_HOME environment variable

--- images/linux/scripts/installers/leiningen.sh
+++ images/linux/scripts/installers/leiningen.sh
@@ -11,6 +11,13 @@ source $HELPER_SCRIPTS/document.sh
 curl -s https://raw.githubusercontent.com/technomancy/leiningen/stable/bin/lein > /usr/local/bin/lein \
   && chmod 0755 /usr/local/bin/lein
 
+## To have the variable set during the build phase
+export LEIN_HOME=/usr/local/lib/lein
+
+## To have the variable set during the run phase
+echo "export LEIN_HOME=/usr/local/lib/lein" >> /etc/profile.d/leiningen.sh
+chmod +x /etc/profile.d/leiningen.sh
+
 ## Document what was added to the image
 echo "Lastly, documenting what we added to the metadata file"
 DocumentInstalledItem "Leiningen version ($(lein -v))"
